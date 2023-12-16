ecedd1b449903b5c21b6e43b19bad39354493cc9
FairyPenguin <autorun81@gmail.com>
2023-12-16 04:34:23 +0200

    deleteMany

--- myApp.js
+++ myApp.js
@@ -127,7 +127,7 @@ const removeById = (personId, done) => {
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-  
+
   Person.deleteMany({ name: nameToRemove }, function (err, response) {
     if (err) return console.log(err);
     done(null, response);
@@ -136,8 +136,13 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  Person.find({ favoriteFoods: foodToSearch })
+    .sort({ name: 1 })
+    .limit(2)
+    .select({ age: 0 })
+    .exec(function (err, people) {
+      done(null, data);
+    });
 };
 
 /** **Well Done !!**
